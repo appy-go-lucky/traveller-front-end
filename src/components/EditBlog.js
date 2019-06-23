651f4c015dd2544f65dbe6eb88e208620b344376
Jo Preihs <jo_preihs@hotmail.com>
2019-06-23 15:27:15 +0100

    Frontend with more edits and footer

--- src/components/EditBlog.js
+++ src/components/EditBlog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+//import './EditBlog.css'; - Not working for some reason
 const countryList = require('country-list');
 
 class EditBlogs extends Component{
